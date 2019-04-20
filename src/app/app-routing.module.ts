464c9e3ef28d2fa474bb8ba6bd122c43f5e03e91
Kay Hudson <kay.hudson@galvanize.com>
2019-04-20 13:22:46 -0400

    Remove routes for exercise.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
-import { SubscribeFormComponent } from './subscribe-form/subscribe-form.component';
 
-const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginFormComponent },
-  { path: 'signup', component: SignupFormComponent },
-  { path: 'subscribers', component: SubscribeFormComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
